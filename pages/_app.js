5e96a48b1533adb11bfeea4ab42a5d9c881a9925
Mark Miro <markmirony+github@gmail.com>
2021-04-06 16:53:09 -0700

    Update nav styles

--- pages/_app.js
+++ pages/_app.js
@@ -3,7 +3,12 @@ import Link from "next/link";
 export default function App({ Component }) {
   return (
     <div>
-      <nav>
+      <nav
+        style={{
+          background: "#ccc",
+          padding: "5px"
+        }}
+      >
         <Link href="/">
           <a>Home</a>
         </Link>{" "}
